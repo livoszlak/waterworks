de37cea8e1ab262ff693f5744943208116ade58a
Liv <110914989+livoszlak@users.noreply.github.com>
2024-02-22 17:43:18 +0100

    Fixed SG value subtraction logic

--- js/modules/lineChartModule.js
+++ js/modules/lineChartModule.js
@@ -1,15 +1,47 @@
 import { fetchDates } from "./apiModule.js";
 
+const measureSites = [
+  { key: "Agnesberg", value: 0 },
+  { key: "Arketjarn", value: 54.54 },
+  { key: "Eriksberg", value: 0 },
+  { key: "Garda", value: 1.25 },
+  { key: "Harsjo", value: 89.46 },
+  { key: "Kalleredsbacken", value: 7.76 },
+  { key: "Landvatter", value: 54.54 },
+  { key: "Levgrensvagen", value: 1.25 },
+  { key: "Larjean", value: 0 },
+  { key: "MolndalCentrum", value: 1.25 },
+  { key: "Nedsjon", value: 0 },
+  { key: "Rada", value: 49.14 },
+  { key: "Skars led", value: 1.25 },
+  { key: "Slussen", value: 1.25 },
+  { key: "Stensjon", value: 49.14 },
+  { key: "Tingstad", value: 0 },
+  { key: "Torshamnen", value: 0 },
+];
+
+var measureSite = sessionStorage.getItem("site");
+
+function findValueByKey(measureSite) {
+  for (let i = 0; i < measureSites.length; i++) {
+    if (measureSites[i].key === measureSite) {
+      return measureSites[i].value;
+    }
+  }
+}
+
+var modifier = findValueByKey(measureSite);
+
 export async function createLineChart() {
   const lineData = await fetchDates();
 
-  var threeMonths = [];
+  var dataArray = [];
   lineData.forEach((element) => {
-    threeMonths.push(element["Value"]);
+    dataArray.push(element["Value"] - modifier);
   });
 
   var labels = [];
-  for (let i = 1; i < threeMonths.length + 1; i++) {
+  for (let i = 1; i < dataArray.length + 1; i++) {
     labels.push(i);
   }
   console.log(labels);
@@ -23,7 +55,7 @@ export async function createLineChart() {
           label: "Water levels",
           backgroundColor: "rgb(255,   99,   132)",
           borderColor: "rgb(255,   99,   132)",
-          data: threeMonths,
+          data: dataArray,
         },
       ],
     },
