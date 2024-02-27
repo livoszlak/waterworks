2d4f1919e449906d430876d6c2b38c0e463b2090
hugosandsjo <hugosandsjo@gmail.com>
2024-02-27 08:47:26 +0100

    not sure if needed

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-// import { fetchDates, formatDate } from "./modules/apiModule";
+import { fetchDates, formatDate } from "./modules/apiModule";
 
-// window.onload = function () {
-//   const btns = document.querySelectorAll("dateButton");
-//   /*   const weeksBtn = document.querySelector("weeksBtn");
-//   const monthsBtn = document.querySelector("monthsBtn");
-//   const yearBtn = document.querySelector("yearBtn"); */
-//   const dropDownTag = document.getElementById("locationDropdown");
+window.onload = function () {
+  const btns = document.querySelectorAll("dateButton");
+  /*   const weeksBtn = document.querySelector("weeksBtn");
+  const monthsBtn = document.querySelector("monthsBtn");
+  const yearBtn = document.querySelector("yearBtn"); */
+  const dropDownTag = document.getElementById("locationDropdown");
 
-//   dropDownTag.addEventListener("change", () => {
-//     site = dropDownTag.value;
-//   });
+  dropDownTag.addEventListener("change", () => {
+    site = dropDownTag.value;
+  });
 
-//   btns.forEach(button, function () {
-//     button.addEventListener("click", () => {});
-//   });
+  btns.forEach(button, function () {
+    button.addEventListener("click", () => {});
+  });
 
-//   const weeksBtn = document.querySelector("#weeksBtn");
-//   const monthsBtn = document.querySelector("#monthsBtn");
-//   const yearBtn = document.querySelector("#yearBtn");
+  const weeksBtn = document.querySelector("#weeksBtn");
+  const monthsBtn = document.querySelector("#monthsBtn");
+  const yearBtn = document.querySelector("#yearBtn");
 
-//   weeksBtn.addEventListener("click", () => {
-//     fromDate = formatDate(weeksBtn.value);
-//     console.log(fromDate);
-//     console.log(weeksBtn.value);
-//     fetchDates(fromDate, site);
-//   });
-// };
+  weeksBtn.addEventListener("click", () => {
+    fromDate = formatDate(weeksBtn.value);
+    console.log(fromDate);
+    console.log(weeksBtn.value);
+    fetchDates(fromDate, site);
+  });
+};
